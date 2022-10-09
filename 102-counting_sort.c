803b1ad284b9d569425de29a86c4a6b055bde14d
JOgr8t <jonathanKingoche@gmail.com>
2022-10-08 19:06:28 -0700

    my commit of a file

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,52 +1,59 @@
 #include "sort.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
-  * counting_sort - Afunction that sorts an array using counting algorithm.
-  * @array: The array to sort.
-  * @size: The length of the array.
-  * Return: Nothing.
-  */
+ * arr_max - array max
+ * @array: array
+ * @size: size of the array
+ * Return: max
+ */
+int arr_max(int *array, size_t size)
+{
+	int max;
+	size_t i;
+
+	max = array[0];
+	for (i = 1; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
+	return (max);
+}
+
+/**
+ * counting_sort - sorts an array with the Counting sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ */
 void counting_sort(int *array, size_t size)
 {
-	unsigned int i = 1;
-	int *counter = NULL, k = 0, j = 0;
+	int *arr, *o_arr, max, num;
+	size_t i;
 
-	if (array == NULL || size < 2)
+	if (size < 2 || !array)
 		return;
+	max = arr_max(array, size);
 
-	k = array[0];
-	for (; i < size; i++)
-	{
-		if (array[i] > k)
-			k = array[i];
-	}
+	arr = malloc(sizeof(size_t) * (max + 1));
+	o_arr = malloc(sizeof(int) * size);
 
-	counter = malloc(sizeof(int) * (k + 1));
-	if (counter == NULL)
-		return;
-
-	for (j = 0; j <= k; j++)
-		counter[j] = 0;
-	for (i = 0; i < size; i++)
-		counter[array[i]] += 1;
-	for (j = 0; j < k; j++)
-	{
-		counter[j + 1] += counter[j];
-		printf("%d, ", counter[j]);
-	}
-	counter[j + 1] += counter[j];
-	printf("%d\n", counter[j + 1]);
+	for (i = 0; (int)i <= max; i++)
+		arr[i] = 0;
 	for (i = 0; i < size; i++)
 	{
-		j = counter[array[i]] - 1;
-		if (array[i] != array[j])
-		{
-			k = array[i];
-			array[i] = array[j];
-			array[j] = k;
-		}
+		num = array[i];
+		arr[num] += 1;
 	}
-	free(counter);
+	for (i = 1; (int)i <= max; i++)
+		arr[i] += arr[i - 1];
+	print_array(arr, max + 1);
+	for (i = 0; i < size; i++)
+	{
+		o_arr[arr[array[i]] - 1] = array[i];
+		arr[array[i]]--;
+	}
+	for (i = 0; i < size; i++)
+		array[i] = o_arr[i];
+
+	free(o_arr);
+	free(arr);
 }
